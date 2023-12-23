2bcfe617b51363c2e5b52bb28847cd4ce58e26a7
cooperlkeenan <70544610+cooperlkeenan@users.noreply.github.com>
2023-12-23 20:17:13 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,11 +4,14 @@
 build_and_run() {
     local docker_dir=$1
     local container_name=$2
+    echo "Building image for $container_name..."
 
     # Navigate to the Docker directory and build the image
     cd "$docker_dir" || exit
     docker build -t "$container_name-image" . > /dev/null 2>&1 #stops detailed output 
-    
+
+    #run container
+    echo "Running $container_name container..."
     docker run -dit --name "$container_name" "$container_name-image" > /dev/null 2>&1
     echo "$container_name created"
     cd - > /dev/null 2>&1
@@ -86,29 +89,41 @@ round_robin sorted_files_docker1 "docker1-container"
 round_robin sorted_files_docker2 "docker2-container"
 round_robin files_docker3 "docker3-container"
 echo "Finished loading text..."
+echo "The Game of Dockers Chapter has been created."
+
 
 # Terminal user interface 
-echo "The Game of Dockers Chapter has been created."
-echo "Would you like to read Game of Dockers Chapter? (Yes/No)"
-read read_answer
 
-if [ "$read_answer" == "Yes" ]; then
+# validate user input
+validate_input() {
+    while true; do
+        read -p "$1 (Y/N): " answer
+        case $answer in
+            [Yy] ) echo "Y"; break;;  # If input = Y return "Y"
+            [Nn] ) echo "N"; break;;  # If input = Y return "Y"
+            * ) echo "Please answer Y or N.";  # Request valid input
+        esac
+    done
+}
+
+# Read and validate user input for reading the chapter
+read_answer=$(validate_input "Would you like to read Game of Dockers Chapter?")
+if [ "$read_answer" == "Y" ]; then
     cat FinalChapter.txt
 fi
 
-echo "Would you like to add any text to Game of Dockers? (Yes/No)"
-read add_answer
-
-if [ "$add_answer" == "Yes" ]; then
+# Read and validate user input for adding text
+add_answer=$(validate_input "Would you like to add any text to Game of Dockers?")
+if [ "$add_answer" == "Y" ]; then
     echo "Enter the text you want to add (finish with CTRL+D):"
     cat >> FinalChapter.txt
 fi
 
-echo -e "\nWould you like to remove any text from Game of Dockers? (Yes/No)"
-read remove_answer
-
-if [ "$remove_answer" == "Yes" ]; then
+# Read and validate user input for removing text
+remove_answer=$(validate_input "Would you like to remove any text from Game of Dockers?")
+if [ "$remove_answer" == "Y" ]; then
     sudo nano FinalChapter.txt
 fi
 
 exit 0
+
